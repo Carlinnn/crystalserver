e34db867105b5907f493a63e3da15d41d2744c75
Carlinnn <carlinomaisbrabo@gmail.com>
2026-01-06 21:51:41 -0400

    feat: add animated animated text and effects for specific map positions

---
+++ data/scripts/globalevents/teleport_texts.lua
@@ -0,0 +1,28 @@
+local effects = {
+    {position = Position(480, 494, 7), text = 'Bem Vindo ao GordonsOT', effect = CONST_ME_HOLYAREA},
+    {position = Position(482, 488, 7), text = 'TRAINERS', effect = CONST_ME_WATERCREATURE},
+    {position = Position(480, 487, 7), text = 'HUNTS', effect = CONST_ME_GREENSMOKE},
+    {position = Position(478, 488, 7), text = 'RAID', effect = CONST_ME_PURPLEENERGY},
+}
+
+local animatedText = GlobalEvent("AnimatedText") 
+function animatedText.onThink(interval)
+    for i = 1, #effects do
+        local settings = effects[i]
+        local spectators = Game.getSpectators(settings.position, false, true, 7, 7, 5, 5)
+        if #spectators > 0 then
+            if settings.text then
+                for i = 1, #spectators do
+                    spectators[i]:say(settings.text, TALKTYPE_MONSTER_SAY, false, spectators[i], settings.position)
+                end
+            end
+            if settings.effect then
+                settings.position:sendMagicEffect(settings.effect)
+            end
+        end
+    end
+   return true
+end
+
+animatedText:interval(4550)
+animatedText:register()
